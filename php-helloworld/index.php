05ad58e3558f74f503cc96bf91e93b16c9857fb9
Student User <student@workstation.lab.example.com>
2020-08-22 06:26:49 +0000

    samedi

--- php-helloworld/index.php
+++ php-helloworld/index.php
@@ -1,8 +1,4 @@
 <?php
 print "Hello, World! php version is " . PHP_VERSION . "\n";
-<<<<<<< HEAD
-print "A change is a coming !\n";
-=======
-print "A change is in the air!\n";
->>>>>>> d419087f5b297e250b18dec0f419c4841690c802
+print "On est samedi !\n"
 ?>
